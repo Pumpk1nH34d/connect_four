f79526e2ea82b847ec17bd289a31571c5f56a1ad
Pumpk1nH34d <delic.austin@gmail.com>
2023-03-25 22:41:39 +0800

    initialize

---
+++ ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  connect_four
+//
+//  Created by Au?tin on 25/3/2023.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ connect_fourApp.swift
@@ -0,0 +1,18 @@
+//
+//  connect_fourApp.swift
+//  connect_four
+//
+//  Created by Au?tin on 25/3/2023.
+//
+//
+
+import SwiftUI
+
+@main
+struct connect_fourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
