3626f7fb469ba40940b4599aae0bbfb32c95e8d4
Pumpk1nH34d <delic.austin@gmail.com>
2023-03-25 22:59:04 +0800

    test

--- ContentView.swift
+++ ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("test 123 123")
+            Text("ur mother is a nigga")
         }
         .padding()
     }
